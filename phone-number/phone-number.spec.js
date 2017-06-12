ab8cefd60eb23e00950e57bd6cd0d3203c0e7eee
Persianparadoxic <persianparadoxic@gmail.com>
2017-06-12 17:02:59 -0400

    worksofar

--- phone-number/phone-number.spec.js
+++ phone-number/phone-number.spec.js
@@ -9,60 +9,64 @@ var PhoneNumber = require('./phone-number');
  * good examples of proper form.
  */
 
-describe('', function () {
+describe('number()', function () {
 
-  it('', function () {
+  it('should remove spaces, parentheses, and dashes from 10 digits starting with 1', function () {
     var phone = new PhoneNumber('(123) 456-7890');
     expect(phone.number()).toEqual('1234567890');
   });
 
-  it('', function () {
+  it('should remove (.)s from 10 digits stating with 1', function () {
     var phone = new PhoneNumber('123.456.7890');
     expect(phone.number()).toEqual('1234567890');
   });
 
-  it('', function () {
+  it('should remove (.s) from 10 digits starting with !1', function () {
     var phone = new PhoneNumber('555.456.7890');
     expect(phone.number()).toEqual('5554567890');
   });
 
-  it('', function () {
+  it('should remove the 1 in front of an 11 digit number', function () {
     var phone = new PhoneNumber('11234567890');
     expect(phone.number()).toEqual('1234567890');
   });
 
-  it('', function () {
+  it('should return 00.... for an 11 digit number !starting with 1', function () {
     var phone = new PhoneNumber('2  1234567890');
     expect(phone.number()).toEqual('0000000000');
   });
 
-  it('', function () {
+  it('should return 00.... for a 12+ digit number !starting with 1', function () {
     var phone = new PhoneNumber('991234567890');
     expect(phone.number()).toEqual('0000000000');
   });
 
-  it('', function () {
+  it('should return 00.... to a number less than 9 digits', function () {
     var phone = new PhoneNumber('123456789');
     expect(phone.number()).toEqual('0000000000');
   });
+});
 
-  it('', function () {
+describe('areaCode()', function () {
+
+  it('should return the first three digits of a 10 digit number starting with 1', function () {
     var phone = new PhoneNumber('1234567890');
     expect(phone.areaCode()).toEqual('123');
   });
 
-  it('', function () {
+  it('should return the first three digits of a 10 digit number starting with !1', function () {
     var phone = new PhoneNumber('5554567890');
     expect(phone.areaCode()).toEqual('555');
   });
+});
 
-  it('', function () {
-    var phone = new PhoneNumber('1234567890');
-    expect(phone.toString()).toEqual('(123) 456-7890');
-  });
-
-  it('', function () {
+describe('toString()', function () {
+  it('should add parenthesis around the first three digits of a 10 digit number !starting with 1, add a space, then the next three numbers, a dash, and the final four digits.', function () {
     var phone = new PhoneNumber('5554567890');
     expect(phone.toString()).toEqual('(555) 456-7890');
+  });
+    it('should add parenthesis around the first three digits of a 10 digit number starting with 1, add a space, then the next three numbers, a dash, and the final four digits.', function () {
+    var phone = new PhoneNumber('1234567890');
+    expect(phone.toString()).toEqual('(123) 456-7890');
   });
 });
